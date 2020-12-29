c107f32f494481294a9af75747250eb9bcd8bd2e
Krzysztof Major <krzysztof.major@gmail.com>
2020-12-29 13:53:19 +0000

    nuget pack

--- pack.ps1
+++ pack.ps1
@@ -1,3 +1,2 @@
-$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
-Write-Host "Setting .nuspec version tag to $root"
-$root\NuGet\NuGet.exe pack
+Write-Host "Setting .nuspec version tag to"
+./NuGet.exe pack
